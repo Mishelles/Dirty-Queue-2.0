66f9c7ca42ca09b408c576a329b71c13550a27ff
root <root@7dd18f2b56a5>
2017-10-15 14:47:00 +0000

    added status column to work list search

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,14 +1,15 @@
 function doSearch() {
-  var input, filter, table, tr, td1, td2, i, text;
+  var input, filter, table, tr, td1, td2, td3, i, text;
   input = document.getElementById("dosearch");
   filter = input.value.toUpperCase();
   table = document.getElementById("works");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
     td1 = tr[i].getElementsByTagName("td")[0];
-	td2 = tr[i].getElementsByTagName("td")[1];
-    if (td1 && td2) {
-	  text = td1.innerHTML + ' ' + td2.innerHTML;
+    td2 = tr[i].getElementsByTagName("td")[1];
+    td3 = tr[i].getElementsByTagName("td")[2];
+    if (td1 && td2 && td3) {
+	  text = td1.innerHTML + ' ' + td2.innerHTML + ' ' + td3.innerHTML;
       text = text.replace(/\n/g, "").replace(/  +/g, ' ');
       if (text.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
